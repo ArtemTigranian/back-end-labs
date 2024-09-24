ac5466aa806e46bf299c051b0d50f4823d2697ce
Артём Тигранян <artem.tigranianp@gmail.com>
2024-09-24 13:03:25 +0700

    Исправление адресов

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, url_for, redirect
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/web")
+@app.route("/lab1/web")
 def web():
     return """<!doctype html>
         <html>
@@ -17,7 +17,7 @@ def web():
             'Content-Type':'text/plain; charset=utf-8'
         }
 
-@app.route("/author")
+@app.route("/lab1/author")
 def author():
     name = "Тигранян Артём Паруйрович"
     group = "ФБИ-21"
@@ -84,9 +84,9 @@ def reset_counter():
 </html>
 '''
 
-@app.route("/info")
+@app.route("/lab1/info")
 def info():
-    return redirect("/author")
+    return redirect("/lab1/author")
 
 @app.route("/lab1/created")
 def created():
