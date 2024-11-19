de7f8facde749fe456e96a1156cf818a661ccec1
Артём Тигранян <artem.tigranianp@gmail.com>
2024-11-19 21:03:09 +0700

    Хеширование паролей

--- lab5.py
+++ lab5.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, url_for, redirect, render_template, request, make_response, session
 import  psycopg2
 from psycopg2.extras import RealDictCursor
-
+from werkzeug.security import check_password_hash, generate_password_hash
 lab5 = Blueprint('lab5', __name__)
 
 @lab5.route('/lab5/')
@@ -25,6 +25,7 @@ def db_close(conn, cur):
     cur.close()
     conn.close()
 
+
 @lab5.route('/lab5/register', methods = ['GET', 'POST'])
 def register():
     if request.method == 'GET':
@@ -33,7 +34,7 @@ def register():
     login = request.form.get('login')
     password = request.form.get('password')
 
-    if not (login or password):
+    if not login or not password:
         return render_template('lab5/register.html', error = 'Заполните все поля!')
     
     conn, cur = db_connect()
@@ -43,7 +44,8 @@ def register():
         db_close(conn, cur)
         return render_template('lab5/register.html', error='Такой пользователь уже существует')
     
-    cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password}');")
+    password_hash = generate_password_hash(password)
+    cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password_hash}');")
     
     db_close(conn, cur) 
     return render_template('lab5/success.html', login=login)
@@ -57,7 +59,7 @@ def login():
     login = request.form.get('login')
     password = request.form.get('password')
 
-    if not (login or password):
+    if not login or not password:
         return render_template('lab5/register.html', error = 'Заполните все поля!')
 
     conn = psycopg2.connect(
@@ -73,6 +75,10 @@ def login():
 
     if not user:
         db_close(conn, cur) 
+        return render_template('lab5/login.html', error='Логин и/или пароль неверны')
+    
+    if not check_password_hash(user['password'], password):
+        db_close(conn,cur)
         return render_template('lab5/login.html', error='Логин и/или пароль неверны')
     
     if user['password'] != password:
