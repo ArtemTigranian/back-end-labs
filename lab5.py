651b0d9163cf5efe211567ec9b3d2c19fdde28e9
Артём Тигранян <artem.tigranianp@gmail.com>
2024-11-19 18:42:05 +0700

    Регистрация

--- lab5.py
+++ lab5.py
@@ -1,7 +1,40 @@
 from flask import Blueprint, url_for, redirect, render_template, request, make_response, session
+import  psycopg2
 lab5 = Blueprint('lab5', __name__)
 
 @lab5.route('/lab5/')
 def lab():
     name = 'Anonymous'
     return render_template('lab5/lab5.html', name=name)
+
+
+@lab5.route('/lab5/register', methods = ['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('lab5/register.html')
+    
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if not (login or password):
+        return render_template('lab5/register.html', error = 'Заполните все поля!')
+    
+    conn = psycopg2.connect(
+        host = '127.0.0.1',
+        database = 'artem_tigranian_knowledge_base',
+        user = 'artem_tigranian_knowledge_base',
+        password = 'artem'
+    )
+    cur = conn.cursor()
+
+    cur.execute(f"SELECT login FROM users WHERE login='{login}';")
+    if cur.fetchone():
+        cur.close()
+        conn.close()
+        return render_template('lab5/register.html', error='Такой пользователь уже существует')
+    
+    cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password}');")
+    conn.commit()
+    cur.close()
+    conn.close()
+    return render_template('lab5/success.html', login=login)
