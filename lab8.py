8c93b25cec4e6ec80b74fd1fed008873c39c9876
Артём Тигранян <artem.tigranianp@gmail.com>
2024-12-17 19:44:20 +0700

    Логаут

--- lab8.py
+++ lab8.py
@@ -6,13 +6,13 @@ import sqlite3
 from os import path
 from db import db
 from db.models import users, articles
-from flask_login import login_user, login_required, current_user
+from flask_login import login_user, login_required, current_user, logout_user
 
 lab8 = Blueprint('lab8', __name__)
 
 @lab8.route('/lab8/')
 def lab():
-    return render_template('lab8/lab8.html', login=session.get('login'))
+    return render_template('lab8/lab8.html')
 
 @lab8.route('/lab8/register/', methods=['GET', 'POST'])
 def register():
@@ -39,7 +39,7 @@ def register():
     return redirect('/lab8/')
 
 
-@lab8.route('/lab8/login', methods=['GET', 'POST'])
+@lab8.route('/lab8/login/', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
         return render_template('lab8/login.html')
@@ -68,3 +68,11 @@ def login():
 @login_required
 def article_list():
     return 'spisok statey'
+
+
+@lab8.route('/lab8/logout/')
+@login_required
+def logout():
+    session.pop('login', None)
+    logout_user()
+    return redirect('/lab8/')
